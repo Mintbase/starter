aa21ca833b6dd7d35c5f08077552855681c4d0cb
Ruben Marcus <ruben@rubenmarcus.dev>
2023-11-16 17:25:18 +0000

    shims

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,8 +1,10 @@
 import { MintbaseWalletContextProvider } from "@mintbase-js/react";
 import "./App.css";
 import { NearWalletConnector } from "./NearWalletConnector";
+import "./shims";
 
 function App() {
+
   const MintbaseWalletSetup = {
     contractAddress: "hellovirtualworld.mintspace2.testnet",
     network: "testnet",
